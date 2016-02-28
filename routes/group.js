71dd9c2be8b571d580d2c78ca366895ba08dff41
Ari Ingber <ariingber@gmail.com>
2016-02-28 15:42:43 -0500

    a user cna sign in, see there work, check out there group, check out an individual piece of work of one of their friends.  Next feature is comments

--- routes/group.js
+++ routes/group.js
@@ -3,11 +3,6 @@ var group = express.Router();
 var bodyParser = require('body-parser');
 var db = require('./../db/pg');
 
-// group.get('/', db.renderWork, function(req, res) {
-//   res.render('works/work_profile.ejs', { workToRender: res.workToRender
-//   });
-// })
-
 group.get('/', db.getGroupMembers, function(req, res) {
   res.render('./group_profile.ejs', { groupMembers: res.groupMembers
   });
