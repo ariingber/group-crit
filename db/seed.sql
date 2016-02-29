afa877b0267a7eecb7b8e654963503921cf1b19e
Ari Ingber <ariingber@gmail.com>
2016-02-29 15:32:00 -0500

    all working just working on styling

--- db/seed.sql
+++ db/seed.sql
@@ -1,19 +1,17 @@
 BEGIN;
 
-
-UPDATE users SET img_url='https://s-media-cache-ak0.pinimg.com/736x/e1/03/f3/e103f385cd8fa91e1e8a4f6d41dea093.jpg' WHERE name='mikey';
-UPDATE users SET img_url='https://s-media-cache-ak0.pinimg.com/236x/58/f2/2d/58f22d375cd1ff73b08dfd28c4dbd175.jpg' WHERE name='leo';
-UPDATE users SET img_url='https://s-media-cache-ak0.pinimg.com/564x/af/74/98/af74984efa8f65cf91c27e2b62ad85cc.jpg' WHERE name='raph';
-UPDATE users SET img_url='https://s-media-cache-ak0.pinimg.com/736x/be/ca/22/beca2251ca5f4d859abb30ff7527d1cb.jpg' WHERE name='don';
-
 insert into works ( userID, imgURL, name ) VALUES ( 1, 'http://www.theartpole.com/wordpress/wp-content/uploads/2015/01/The-Artpole-Johanna-Calle-Perspectivas-720x555.jpg', 'future man');
-insert into works ( userID, imgURL, name ) VALUES ( 1, 'https://s-media-cache-ak0.pinimg.com/236x/8c/41/fb/8c41fbea36aa909a7255150ac641779f.jpg', 'pizza two');
-insert into works ( userID, imgURL, name ) VALUES ( 2, 'http://www.pizzarules.com/uploads/2010/pizzafacemonsterA.jpg', 'pizza three');
-insert into works ( userID, imgURL, name ) VALUES ( 3, 'http://thinng.com/system/images/6148/large/campfire-tales-of-a-pepperoni-pizza-art-print.png?1323155332', 'pizza four');
-insert into works ( userID, imgURL, name ) VALUES ( 4, 'http://cdn4.designbyhumans.com/product/design/u867762/pr186028-2-2370568-640x640-b-p-313131.jpg', 'pizza five');
+insert into works ( userID, imgURL, name ) VALUES ( 1, 'http://thumb7.shutterstock.com/display_pic_with_logo/663421/663421,1316016870,1/stock-photo-side-view-of-sitting-human-charcoal-drawing-84643642.jpg', 'human charcoal');
 
+insert into works ( userID, imgURL, name ) VALUES ( 2, 'http://rlv.zcache.com/black_and_white_marble_acrylic_painting_canvas_print-r33584a72d63e448d854e74392c754621_wta_8byvr_630.jpg?view_padding=%5B285%2C0%2C285%2C0%5D', 'accrylic');
+insert into works ( userID, imgURL, name ) VALUES ( 2, 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Fleur_de_muscade.jpg', 'sculpture test');
 
+insert into works ( userID, imgURL, name ) VALUES ( 3, 'https://wegway.files.wordpress.com/2013/04/screw.jpg', 'screw drawing');
+insert into works ( userID, imgURL, name ) VALUES ( 3, 'http://s3images.coroflot.com/user_files/individual_files/original_255014_dt5Oj1kUOTGT1Cew8bRhb07_1.jpg', 'tape drawing');
+insert into works ( userID, imgURL, name ) VALUES ( 3, 'http://www.mollynix.com/images_content/04tradmedia/bell01.jpg', 'tape drawing');
 
+insert into works ( userID, imgURL, name ) VALUES ( 4, 'http://i.dailymail.co.uk/i/pix/2014/06/22/article-2665197-1F0402AB00000578-314_964x613.jpg', 'animals');
+insert into works ( userID, imgURL, name ) VALUES ( 4, 'https://c2.staticflickr.com/8/7040/7068402247_6869ee0680_b.jpg', 'more animals');
 
 
 COMMIT;
